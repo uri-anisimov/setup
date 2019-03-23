e3191eb2be04931898ad57fe912b8536826d0388
yuriapxlt <yuria@pixellot.tv>
2019-03-23 18:21:20 +0200

    robot script update

--- install/robot.sh
+++ install/robot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 sudo apt -y install python3-pip
+sudo apt -y install python-pip
 sudo pip install --upgrade pip
 sudo pip install robotframework
 sudo pip install --upgrade robotframework
